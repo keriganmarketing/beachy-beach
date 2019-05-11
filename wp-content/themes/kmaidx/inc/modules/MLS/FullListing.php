73f3962f115d40fd0730ab81eaecf908e306b7b9
programmer-man <bryan@kerigan.com>
2019-05-11 18:03:12 -0400

    add referrer to guzzle geader

--- wp-content/themes/kmaidx/inc/modules/MLS/FullListing.php
+++ wp-content/themes/kmaidx/inc/modules/MLS/FullListing.php
@@ -24,7 +24,13 @@ class FullListing
 
     public function create()
     {
-        $client = new Client(['base_uri' => 'https://mothership.kerigan.com/api/v1/listing/','http_errors' => false]);
+        $client = new Client([
+            'base_uri' => 'https://mothership.kerigan.com/api/v1/listing/',
+            'http_errors' => false,
+            'headers' => [
+                'Referrer' => $_SERVER['HTTP_USER_AGENT']
+            ]
+        ]);
 
         // make the API call
         $raw = $client->request(
@@ -33,33 +39,8 @@ class FullListing
         );
 
         $results = json_decode($raw->getBody());
-
         return $results;
     }
-
-//     public function isOurs($listingInfo)
-//     {
-//         $agents = new Agents();
-//         $agentArray = $agents->getTeam();
-
-//         $mlsArray = array();
-//         foreach ($agentArray as $agent) {
-
-//             $agentIds = explode(',',$agent['short_ids']);
-//             foreach($agentIds as $agentId){
-//                 $mlsArray[] = $agentId;
-//             }
-
-//         }
-
-//         if (in_array($listingInfo->listing_member_shortid, $mlsArray) ||
-//             in_array($listingInfo->colisting_member_shortid, $mlsArray)
-//         ) {
-//             return true;
-//         }
-
-//         return false;
-//     }
 
 	public function isOurs($listingInfo)
     {
