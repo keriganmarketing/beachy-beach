bd3257a5945e0ed4f2fe6a384415286a6c5ee68f
programmer-man <bryan@kerigan.com>
2019-07-05 10:24:51 -0400

    add mortgage calculator

--- wp-content/themes/kmaidx/page-mortgage-calculator.php
+++ wp-content/themes/kmaidx/page-mortgage-calculator.php
@@ -26,7 +26,45 @@ get_header(); ?>
         </main><!-- #main -->
     </div><!-- #primary -->
 </div>
-<?php get_template_part( 'template-parts/mls', 'mortgage-calulator' ); ?>
+<div id="mortgage-calculator" style="width:100%;">
+	<div class="container">
+        <div id="mortgage-caculator-box" >
+		<form name="mortcalc" id="mortform" >
+            <div class="row justify-content-center no-gutters">
+                <div id="col1" class="col-md-6 col-lg-4">
+                    <label>Home Price</label> <input type="text" name="balance" id="balance" value="300000"  ><br>
+                    <label>Down payment</label> <input type="text" name="down_payment" id="downpayment" value="60000" onChange="updatePercent();" ><input type="text" name="down-pay_percent" id="downpaypercent" value="" onChange="updatePayment();" ><br>
+                    <label>Interest rate</label> <input type="text" name="rate" id="rate" value="3.92" ><br>
+                    <label>Loan term</label> <input type="text" name="term" id="term" value="360" placeholder="360 months"  >
+                </div>
+                <div id="col2" class="col-md-7 col-lg-4">
+                    <canvas id="mortCal" style="min-height:200px;"></canvas>
+                    <div id="payment"><span id="mon_payment">$<span id="payment_num">1,721</span></span></div>
+                    <div id="labels">
+                        <div id="left">
+                            <div id="hoa"></div>
+                            <div id="tax"></div>
+                        </div>
+                        <div id="right">
+                            <div id="pi"></div>
+                            <div id="ins"></div>
+                        </div>
+                    </div>
+                    <div id="cal-button"><span class="calculate" onClick="mortCal();">Calculate</span></div>
+                </div>
+                <div id="col3" class="col-md-6 col-lg-4">
+                    <label class="checkboxlabel">Include taxes</label> <span class="checkbox-container"><input type="checkbox" name="include_taxes" id="includetaxes" checked="checked" ></span><br>
+                    <label>Property taxes</label> <input type="text" name="prop_taxes" id="proptaxes" value="1.2" ><br>
+                    <label id="homeinsurancelabel">Home insurance</label> <input type="text" name="home_insurance" id="homeinsurance" value="800" ><br>
+                    <label class="checkboxlabel">Include HOA dues</label><span class="checkbox-container"><input type="checkbox" name="include_pmi" id="includepmi" checked="checked" ></span><br>
+                    <label> HOA dues</label> <input type="text" name="hoa_dues" id="hoadues" value="200" >
+                </div>
+            </div>
+			<input type="submit" onClick="mortCal();this.preventDefault();" style="visibility:hidden; display: block; height:1px; width:1px; margin:0; padding:0;"  >
+		</form>
+        </div>
+	</div>
+</div>
 <?php 
 wp_enqueue_script( 'chart-js' );
 wp_enqueue_script( 'mortgage-calc' );
