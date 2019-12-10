18a799d8af4afb1ac10f8e711721ad5d30039b4d
Alex <alex@kerigan.com>
2019-12-10 16:48:56 -0500

    Site by KMA is now underlined by default

--- wp-content/themes/kmaidx/footer.php
+++ wp-content/themes/kmaidx/footer.php
@@ -82,7 +82,7 @@ use Includes\Modules\Social\SocialSettingsPage;
                         <p class="siteby"><svg version="1.1" id="kma" height="16" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 12.5 8.7" style="enable-background:new 0 0 12.5 8.7;" xml:space="preserve">
                                 <path fill="#b4be35" class="kma" d="M6.4,0.1c0,0,0.1,0.3,0.2,0.9c1,3,3,5.6,5.7,7.2l-0.1,0.5c0,0-0.4-0.2-1-0.4C7.7,7,3.7,7,0.2,8.5L0.1,8.1
                             c2.8-1.5,4.8-4.2,5.7-7.2C6,0.4,6.1,0.1,6.1,0.1H6.4L6.4,0.1z"></path>
-                        </svg> <a href="https://keriganmarketing.com">Site by KMA</a>.</p>
+                        </svg> <u><a href="https://keriganmarketing.com">Site by KMA</a></u>.</p>
                     </div>
                 </div>
             </div><!-- .container -->
